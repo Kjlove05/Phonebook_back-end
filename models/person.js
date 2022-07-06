786f382ebc450e84b5060fe910c054041456684d
Kyle Joshua <kj1600053@gmail.com>
2022-07-06 15:09:52 +0800

    new build

--- models/person.js
+++ models/person.js
@@ -24,6 +24,14 @@ var personSchema = mongoose.Schema({
             minLength: 8 }
 });
 
+
 personSchema.plugin(uniqueValidator)
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
 
 module.exports = mongoose.model('Person', personSchema)
