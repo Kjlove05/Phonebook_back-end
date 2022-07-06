38f9fbfc78cc822d43d4b826efdc79bc4e38c9b9
Kyle Joshua <kj1600053@gmail.com>
2022-07-06 13:06:06 +0800

    Update index.js

--- index.js
+++ index.js
@@ -172,7 +172,7 @@ app.get('/api/persons/:id', (req, res, next) => {
 
 
 
-  const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
+  const PORT = process.env.PORT 
+  app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
