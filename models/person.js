a7373e7a27f826f657c98a016d410d5bfb4e8a4d
Kyle Joshua <kj1600053@gmail.com>
2022-07-06 15:20:55 +0800

    adding validator

--- models/person.js
+++ models/person.js
@@ -20,8 +20,17 @@ var personSchema = mongoose.Schema({
         minLength: 3 },
         
   number: { type: String, 
-            required: true, 
-            minLength: 8 }
+            
+            minLength: 8,
+            validate: {
+              validator: function(v){
+                return /\d{3}-\d{3}-\d{4}/.test(v);
+              },
+              message: props => `${props.value} is not a valid phone number!`
+    },
+    required: [true, 'User phone number required']
+  }
+            
 });
 
 
