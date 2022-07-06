f08010ebe95cd840562580060832d53d8b11e2e9
Kyle Joshua <kj1600053@gmail.com>
2022-07-06 14:32:21 +0800

    Update person.js

--- models/person.js
+++ models/person.js
@@ -13,23 +13,16 @@ mongoose.connect(url,  { useNewUrlParser: true })
     console.log('error connecting to MongoDB:', error.message)
   })
 
-  const personSchema = new mongoose.Schema({
-    name: {
-      type:String, 
-      minLength: 3,
-      required: true
-  },
-    number: {
-      type:Number, 
-      minLength:8,
-      required: true
-  }
-})
 
-var contentSchema = mongoose.Schema({
-  name: { type: String, required: true, minLength: 3 },
-  number: { type: String, required: true, minLength: 8 }
+var personSchema = mongoose.Schema({
+  name: { type: String, 
+        required: true, 
+        minLength: 3 },
+        
+  number: { type: String, 
+            required: true, 
+            minLength: 8 }
 });
 
-contentSchema.plugin(uniqueValidator)
+personSchema.plugin(uniqueValidator)
 module.exports = mongoose.model('Person', personSchema)
